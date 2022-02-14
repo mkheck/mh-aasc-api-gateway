adb6daf330cb8ad1171d4fc565bc2b2ee27a0f52
Mark Heckler <mark.heckler@gmail.com>
2022-02-14 15:56:18 -0600

    Pending updates

--- src/main/java/com/thehecklers/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/thehecklers/apigateway/ApiGatewayApplication.java
@@ -10,6 +10,8 @@ import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.Duration;
@@ -38,5 +40,13 @@ public class ApiGatewayApplication {
 				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
 				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
 				.build());
+	}
+}
+
+@RestController
+class GatewayController {
+	@GetMapping
+	public final String statusUp() {
+		return "Spring Cloud Gateway status: UP";
 	}
 }
